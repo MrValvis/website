af87437603ba1372392f24dc6bb2c98800bcd8ca
Philip Valvis <60443559+MrValvis@users.noreply.github.com>
2024-04-07 16:31:04 +0300

    Update index.js

--- index.js
+++ index.js
@@ -44,3 +44,18 @@ const myFunc = (navCondition) => {
     }
 }
 
+
+// Part 1 javascript functionality ends here
+$(document).ready(function (){
+    if(!$("#myCanvas").tagcanvas({
+        textColour: "#08fdd8",
+        outlineColour: "transparent",
+        reverse: true,
+        depth: 0.8,
+        maxSpeed: 0.05,
+        weight: true,
+    }, "tags")){
+        // something went wrong hide the canvas container,
+        $("#myCanvasContainer");
+    }
+})
